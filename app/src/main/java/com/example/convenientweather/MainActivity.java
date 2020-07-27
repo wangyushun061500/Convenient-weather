7aee9ae70fa5ddd0651f4c2b9667ce2cf468d72f
YuShun <2305808176@qq.com>
2020-07-27 22:18:42 +0800

    可以正常使用了
    状态栏透明的问题还是没有解决

--- app/src/main/java/com/example/convenientweather/MainActivity.java
+++ app/src/main/java/com/example/convenientweather/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.convenientweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends /*AppCompat*/Activity {
+    private WebView webView;
+    private String mareLoginUrl = "https://m.moji.com";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInsstanceState){
+        super.onCreate(savedInsstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        webView = (WebView) findViewById(R.id.webview1);
+
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.loadUrl(mareLoginUrl);
+
+        //覆盖WebView默认使用第三方或者系统默认浏览器打开网页的行为，使网页用WebView打开
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+    }
+    //按键响应
+    @Override
+    public  boolean onKeyDown(int keyCode, KeyEvent event){
+        if((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()){
+            webView.goBack();
+            return true;
+
+        }
+
+        return  super.onKeyDown(keyCode, event);
     }
 }
+
